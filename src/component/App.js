9f3e1e4b0b47d8f4884c1128b3328a085317690c
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2021-12-08 00:02:02 -0600

    Add files via upload

---
+++ src/component/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
+import "./App.css";
+
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <div className="component-app">
+        <Display
+          value={this.state.next || this.state.total || "0"}
+          operation={this.state.operation}
+        />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
